33c8c153729be7cb1781c1ae3a3f9bb474b36e63
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-04-19 17:20:01 +0800

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-git clone https://github.com/ykxVK8yL5L/luci-theme-vzant
+
 git clone https://github.com/kenzok8/openwrt-packages
 git clone https://github.com/kenzok8/small
-
+git clone https://github.com/ykxVK8yL5L/luci-theme-vzant
+git clone https://github.com/ykxVK8yL5L/mypg
 
 rm -rf ./*/README.md
 rm -rf ./*/.git
